1f167a61799a4d534ec3309771c5279ddc3bf72f
Chamod Nimsara <chamasila8@gmail.com>
2025-07-19 23:56:12 +0530

    Update env-list.js

--- plugins/env-list.js
+++ plugins/env-list.js
@@ -94,7 +94,7 @@ async (conn, mek, m, { from, quoted, reply, isCreator }) => {
         await conn.sendMessage(
             from,
             {
-                audio: { url: 'https://github.com/JawadYT36/KHAN-DATA/raw/refs/heads/main/autovoice/menunew.m4a' },
+                audio: { url: 'https://files.catbox.moe/6figid.mp3' },
                 mimetype: 'audio/mp4',
                 ptt: true
             },
